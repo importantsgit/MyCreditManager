2c54c95bb16c1d32a8823f91e64da01986d155dd
importantsgit <dlwognsdk@tukorea.ac.kr>
2023-04-20 15:28:41 +0900

    add function

--- MyCreditManager/StudentsManager.swift
+++ MyCreditManager/StudentsManager.swift
@@ -7,15 +7,61 @@
 
 import Foundation
 
+enum Response {
+    case success
+    case duplication
+    case notExist
+    case getStudent(student: Student)
+    case failure
+}
+
 class StudentsManager {
     var person: [Student] = []
     
-    func findStudent(name: String) -> Student? {
-        return person.filter{$0.name == name}.first
+    
+    func getStudent(name: String) -> Response {
+        if let student = person.filter({$0.name == name}).first {
+            return .getStudent(student: student)
+        }
+        return .failure
     }
     
-    func removeStudent(name: String) {
-        if let removeIndex = person.firstIndex{$0.name == name} {}
-        person.remove(at: person.distance(from: person.startIndex, to: removeIndex))
+    func addStudent(name: String) -> Response {
+        if !person.contains(where: {$0.name == name}) {
+            person.append(Student(name: name))
+            return .success
+        } else {
+            return .duplication
+        }
+    }
+    
+    func removeStudent(name: String) -> Response {
+        if let removeIndex = person.firstIndex(where: {$0.name == name}) {
+            let student = person.remove(at: person.distance(from: person.startIndex, to: removeIndex))
+            return .success
+        } else {
+            return .notExist
+        }
+    }
+    
+    func addSubject(name: String, subject: String, rating: String)->Response {
+        guard let findIndex = person.firstIndex(where: {$0.name == name}) else {return .notExist}
+        let personIndex = person.distance(from: person.startIndex, to: findIndex)
+        person[personIndex].credits?.updateValue(subject, forKey: rating)
+        return .success
+    }
+    
+    func removeSubject(name: String, subject: String)->Response {
+        guard let findIndex = person.firstIndex(where: {$0.name == name}) else {return .notExist}
+        let personIndex = person.distance(from: person.startIndex, to: findIndex)
+        person[personIndex].credits?.removeValue(forKey: subject)
+        return .success
+    }
+    
+    func viewRatings(name: String)->Response {
+        guard let findIndex = person.firstIndex(where: {$0.name == name}) else {return .notExist}
+        let personIndex = person.distance(from: person.startIndex, to: findIndex)
+        person[personIndex].printRating()
+        return .success
     }
 }
